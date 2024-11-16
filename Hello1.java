271403713c9d0ec3052d535aaa68f733d3b85624
Anjali Neeharika <148841999+AnjaliNeeharika@users.noreply.github.com>
2024-11-16 18:34:07 +0530

    Add files via upload

---
+++ Hello1.java
@@ -0,0 +1,22 @@
+package WithLNwithotuLN;
+
+public class Hello1 {
+
+	public static void main(String[] args) {
+		String S1 = "Yesterday ";
+		String S2 = "we ";
+		String S3 = "have ";
+		String S4 = "rain in ";
+		String S5 = "Hyderabad";
+		
+		System.out.println(S1+S2+S3+S4+S5);
+		
+		System.out.print(S1);
+		System.out.print(S2);
+		System.out.print(S3);
+		System.out.print(S4);
+		System.out.print(S5);
+
+	}
+
+}
